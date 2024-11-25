5a95f384876325ed492e82cdf5b3dd1c2a412507
Sahithi Duppati <sahithiduppati@Sahithis-MacBook-Pro.local>
2024-11-24 22:18:40 -0600

    Problem 70

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Dp.Climbling_Stairs;
 import Dp.Fibonacci;
 
 import java.util.Scanner;
@@ -7,6 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(Fibonacci.method(sc.nextInt()));
+//        System.out.println(Fibonacci.method(sc.nextInt()));
+//        System.out.println(Climbling_Stairs.method(sc.nextInt()));
     }
 }
